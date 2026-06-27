7e194cabf4bc916314244c24f8dd2c543ca195e9
vision <jiwarduski5@gmail.com>
2026-06-27 17:37:19 +0300

    Update database.ts

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,3 +1,6 @@
+// WHAT: Sequelize connection to PostgreSQL
+// IMPORTS: sequelize, models/index.ts (to register all models)
+// USED BY: server.ts (connectDB function called at startup)
 import {Sequelize} from 'sequelize';
 import {env} from './env';
 
